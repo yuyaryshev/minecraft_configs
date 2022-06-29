cc59b96b06f2d40627c3542fbe2218efe3f9ba47
Yuri Yaryshev <yuyaryshev@gmail.com>
2022-06-29 20:21:58 +0300

    - Fixed permissions for sh files

--- git_update_only.sh
+++ git_update_only.sh
@@ -2,3 +2,5 @@ cd /services/minecraft/config && git clean -fd && git restore . && git pull
 cd /services/minecraft/kubejs && git clean -fd && git restore . && git pull
 cd /services/minecraft/mods && git clean -fd && git restore . && git pull
 chown -R minecraft /services/minecraft
+chmod +x /services/minecraft/config/git_update_and_restart.sh
+chmod +x /services/minecraft/config/git_update_only.sh
