b0958c7ed498ad1cca9275a85c0f7f2f667c754f
Yuri Yaryshev <yuyaryshev@gmail.com>
2022-07-01 23:18:10 +0300

    - Added serverconfig folder

--- commit_update.bat
+++ commit_update.bat
@@ -1,5 +1,6 @@
 git pull && git add -A && git commit -m update && git push
 cd ../kubejs && git pull && git add -A && git commit -m update && git push
 cd ../mods && git pull && git add -A && git commit -m update && git push
-
+cd ../mods && git pull && git add -A && git commit -m update && git push
+cd ../saves/servertest/serverconfig && git pull && git add -A && git commit -m update && git push
 pause
