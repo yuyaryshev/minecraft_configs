c0799a4020e0e177647f10504b9ef9f40ebd40e4
Yuri Yaryshev <yuyaryshev@gmail.com>
2022-07-01 20:42:13 +0300

    update

---
+++ commit_update.bat
@@ -0,0 +1,5 @@
+git pull && git add -A && git commit -m update && git push
+cd ../kubejs && git pull && git add -A && git commit -m update && git push
+cd ../mods && git pull && git add -A && git commit -m update && git push
+
+pause
