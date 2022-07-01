0c8f7f988e852832c86e1e0f626bcd56ac89376a
Yuri Yaryshev <yuyaryshev@gmail.com>
2022-07-02 00:01:29 +0300

    - Fixed update.bat

--- update.bat
+++ update.bat
@@ -1,5 +1,6 @@
 git clean -fd && git restore . && git pull
 cd ../kubejs && git clean -fd && git restore . && git pull
 cd ../mods && git clean -fd && git restore . && git pull
+cd ../saves/servertest/serverconfig && git clean -fd && git restore . && git pull
 
 pause
